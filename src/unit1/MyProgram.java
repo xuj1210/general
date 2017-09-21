4f4813d4c01871c2d58a8b0655374f32cdaddcdc
xuj1210 <joshuax1210@gmail.com>
2017-09-21 14:32:22 -0400

    I did something xdddddd!!!

---
+++ src/unit1/MyProgram.java
@@ -0,0 +1,24 @@
+/**
+ * A general program template utilizing codehs ConsoleProgram.
+ * The main method and user input method have been abstracted
+ * for simplification.
+ *
+ * 1) Change this description to appropriately describe the program
+ * 2) Change the class name to match the file name
+ * 3) Refer to documentation for configuring your program to run.
+ *
+ * @author: EDIT: yourName
+ *
+ */
+
+package unit1;   // EDIT: set to appropriate lesson unit for this program
+import codehs.*; // utilize all code in the codehs package
+
+
+public class MyProgram extends ConsoleProgram{  //EDIT: change the class name to match the file name
+
+    public void run() {
+        System.out.println("This is my first program!");
+
+    }
+}
