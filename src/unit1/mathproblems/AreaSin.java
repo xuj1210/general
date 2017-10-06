3a9caaf9816c5db5651b11ae7e4d8fb2140a7d53
xuj1210 <joshuax1210@gmail.com>
2017-10-06 13:23:14 -0400

    Updated AreaSin.java

--- src/unit1/mathproblems/AreaSin.java
+++ src/unit1/mathproblems/AreaSin.java
@@ -10,8 +10,11 @@ public class AreaSin extends ConsoleProgram{
         double sideB = readDouble("Enter the length of the second side: ");
         double containedAngle = readDouble("Enter the degree of the contained angle: ");
 
+        //Convert degrees to angles
+        double radiansAngle = Math.toRadians(containedAngle);
+
         // Calculate the area of the triangle
-        double area = (sideA * sideB * Math.sin(containedAngle) / 2);
+        double area = (sideA * sideB * Math.sin(radiansAngle) / 2);
 
         //Output the answer
         System.out.println("The area of the triangle is " + area);
