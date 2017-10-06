900e7e81a678118bc61a3674dca1a5337cbc9c98
xuj1210 <joshuax1210@gmail.com>
2017-10-06 13:47:06 -0400

    Created live hack templates

---
+++ src/unit1/livehack1/LiveHackProblem1.java
@@ -0,0 +1,15 @@
+package unit1.livehack1;
+import codehs.*;
+
+/**
+ * Live Hack Problem 1
+ * author: J. Xu
+ *
+ */
+public class LiveHackProblem1 extends ConsoleProgram{
+
+    public void run() {
+
+
+    }
+}
---
+++ src/unit1/livehack1/LiveHackProblem2.java
@@ -0,0 +1,15 @@
+package unit1.livehack1;
+import codehs.*;
+
+/**
+ * Live Hack Problem 2
+ * author: J. Xu
+ *
+ */
+public class LiveHackProblem2 extends ConsoleProgram{
+
+    public void run() {
+
+
+    }
+}
