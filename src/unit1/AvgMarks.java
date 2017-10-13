982a921b8241c69eae69cd2019f0e652191c161e
Joshua <joshuax1210@gmail.com>
2017-10-13 13:22:30 -0400

    Added CastingDemo.java

--- src/unit1/AvgMarks.java
+++ src/unit1/AvgMarks.java
@@ -19,4 +19,34 @@ public class AvgMarks extends ConsoleProgram{
 
       }
 
+    public static class Hours extends ConsoleProgram{
+
+        public void run() {
+            // Get number of hours
+            int numberHours = readInt("Number of hours: ");
+            // Calculate days and hours
+            int daysTotal = numberHours / 24;
+            int hoursTotal = numberHours % 24;
+            // Output solutions
+            System.out.println(numberHours + " hours is equal to " + daysTotal + " days and " + hoursTotal + " hours.");
+
+        }
     }
+
+    public static class Minutes extends ConsoleProgram{
+
+        public void run() {
+            // Get number of minutes
+            int numberMinutes = readInt("Number of minutes: ");
+
+            // Calculate number of days, hours, minutes
+            int daysFinal = numberMinutes / 1440;
+            int hoursFinal = (numberMinutes % 1440) / 24;
+            int minutesFinal = (numberMinutes % 1440) / 60;
+
+            // Output answers
+            System.out.println(numberMinutes + " is equal to " + daysFinal + " days " + hoursFinal + " hours and " + minutesFinal + " minutes.");
+
+        }
+    }
+}
