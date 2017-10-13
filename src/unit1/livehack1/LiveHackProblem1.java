7d57475090e102ddb9831e53a4b6d6e5d5b0e3b6
Joshua <joshuax1210@gmail.com>
2017-10-13 14:16:01 -0400

    Finished the live hack problems

--- src/unit1/livehack1/LiveHackProblem1.java
+++ src/unit1/livehack1/LiveHackProblem1.java
@@ -2,15 +2,21 @@ package unit1.livehack1;
 import codehs.*;
 
 /**
- * Live Hack Problem 1
- * author: J. Xu
+ * A program which calculates the area of a regular octagon with the input of a length of one side.
+ * author: Joshua Zhonghua Xu
  *
  */
 public class LiveHackProblem1 extends ConsoleProgram{
 
     public void run() {
+        // Get the side length from user
+        double sideLength = readDouble("Enter the length of side a: ");
 
+        // Calculate the area of the octagon
+        double areaOctagon = (Math.sqrt(2) + 1) * Math.pow(sideLength, 2) * 2;
 
+        // Print the answer
+        System.out.println("The area of a regular octagon with side length " + sideLength + " is " + areaOctagon);
 
     }
 }
--- src/unit1/livehack1/LiveHackProblem2.java
+++ src/unit1/livehack1/LiveHackProblem2.java
@@ -2,13 +2,22 @@ package unit1.livehack1;
 import codehs.*;
 
 /**
- * Live Hack Problem 2
- * author: J. Xu
+ * A program that converts a user inputted Fahrenheit temperature to Celsius Degrees
+ * author: Joshua Zhonghua Xu
  *
  */
 public class LiveHackProblem2 extends ConsoleProgram{
 
     public void run() {
+        // Get fahrenheit input from user
+        double fahrenheit = readDouble("Enter the temperature in fahrenheit: ");
+
+        // Convert fahrenheit to celsius degrees
+        double five = 5;
+        double celsius = (fahrenheit - 32) * (five / 9);
+
+        // Output the solution
+        System.out.println("The temperature in celsius is " + celsius + " degrees.");
 
 
     }
