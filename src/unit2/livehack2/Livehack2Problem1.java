be7de8c25d77574086fda218aabc505db8e9d4a4
Joshua <joshuax1210@gmail.com>
2017-10-31 12:54:02 -0400

    finished livehack problems

--- src/unit2/livehack2/Livehack2Problem1.java
+++ src/unit2/livehack2/Livehack2Problem1.java
@@ -1,12 +1,24 @@
 package unit2.livehack2;
 import codehs.*;
 
+/*
+    A program which calculates the distance between the Athabasca Glacier and a nearby lodge
+    per year.
+    Author: Joshua Xu
+ */
 
 public class Livehack2Problem1 extends ConsoleProgram{
 
     public void run() {
+        //Get futureYear and set initial distance
+        int futureYear = readInt("Enter a year in the future: ");
+        int distance = 800;
 
+        //Create a table which outputs the distance and repeats until it reaches the future year provided
+        for(int year = 2017; year <= futureYear; year++){
+            System.out.println("Year : " + year + ", Glacier Edge Distance: " + distance);
+            distance = distance + 7;
 
-
+        }
     }
 }
--- src/unit2/livehack2/Livehack2Problem2.java
+++ src/unit2/livehack2/Livehack2Problem2.java
@@ -1,12 +1,31 @@
 package unit2.livehack2;
 import codehs.*;
 
+/*
+    A program which calculates the year when the lodge will close after the glacier has receded to much
+    Author: Joshua Xu
+ */
 
 public class Livehack2Problem2 extends ConsoleProgram{
 
     public void run() {
+        //Initialize the currentDistance, currentYear, and the distance when the lodge will close
+        int currentDistance = 800;
+        int currentYear = 2017;
+        int yearClosedDistance = 800 * 2;
 
+        //Adds 7 to the currentDistance and 1 to currentYear until currentDistance has doubled
+        while(currentDistance != yearClosedDistance){
+            if(currentDistance <= yearClosedDistance){
+                currentDistance += 7;
+                currentYear++;
+            }
+            else{
+                break;
+            }
 
-
+        }
+        //Output the year when the lodge will close
+        System.out.println("The year the lodge will close is " + currentYear);
     }
 }
