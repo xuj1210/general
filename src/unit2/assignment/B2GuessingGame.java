0a9822d84dc0b51a6c23a5b725fe210d40cd361d
xuj1210 <joshuax1210@gmail.com>
2017-11-13 13:53:22 -0500

    B2GuessingGame partially done

---
+++ src/unit2/assignment/B2GuessingGame.java
@@ -0,0 +1,20 @@
+package unit2.assignment;
+import codehs.*;
+import java.util.Random;
+
+public class B2GuessingGame extends ConsoleProgram {
+    public void run(){
+        Random newRandom = new Random();
+
+        int secretNumber = newRandom.nextInt(100 + 1) + 1;
+        int userGuess = readInt("Guess a number: ");
+        int counter = 0;
+
+        while(userGuess != secretNumber || counter > 5){
+            if(userGuess > secretNumber){
+                System.out.println("Too high, guess again!");
+            }
+        }
+
+    }
+}
