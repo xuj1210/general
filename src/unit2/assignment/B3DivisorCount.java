23afcac844821d784f05b13f91abafb7c7c3fef5
xuj1210 <joshuax1210@gmail.com>
2017-11-14 12:28:19 -0500

    B3DIvisorCount finished

--- src/unit2/assignment/B3DivisorCount.java
+++ src/unit2/assignment/B3DivisorCount.java
@@ -9,7 +9,7 @@ public class B3DivisorCount extends ConsoleProgram {
         int userNumber = readInt("Enter an integer you want the divisors for: ");
         System.out.println("The divisors of " + userNumber + " are:");
 
-        //
+        //For loop to find and check divisors of userNumber
         for(int counter = 1; counter <= userNumber; counter++){
             if(userNumber % counter == 0){
                 System.out.println(counter);
