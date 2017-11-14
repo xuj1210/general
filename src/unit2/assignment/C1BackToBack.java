73e3cf9742b49292f0722bb8a5487878fa45b5dd
xuj1210 <joshuax1210@gmail.com>
2017-11-14 12:53:37 -0500

    C1BackToBack finished input and partial output

---
+++ src/unit2/assignment/C1BackToBack.java
@@ -0,0 +1,13 @@
+package unit2.assignment;
+import codehs.*;
+
+
+public class C1BackToBack extends ConsoleProgram {
+
+    public void run() {
+        String userString = readLine("Enter a string: ");
+        String lastLetter = userString.substring(userString.length());
+
+        System.out.println(lastLetter + userString);
+    }
+}
