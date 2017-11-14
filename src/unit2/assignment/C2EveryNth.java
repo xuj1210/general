0a27935c72e24a6fac7b7dee7d3ee852c308d8d9
xuj1210 <joshuax1210@gmail.com>
2017-11-14 13:09:04 -0500

    C2EveryNth added userinputs

---
+++ src/unit2/assignment/C2EveryNth.java
@@ -0,0 +1,14 @@
+package unit2.assignment;
+import codehs.*;
+
+
+public class C2EveryNth extends ConsoleProgram {
+
+    public void run() {
+        String userString = readLine("Enter a string: ");
+        int everyNth = readInt("Enter an integer: ");
+
+
+
+    }
+}
