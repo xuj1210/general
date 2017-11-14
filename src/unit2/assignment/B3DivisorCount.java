2e0ce3626fc7f7a12be8795b3216e62921b45a06
xuj1210 <joshuax1210@gmail.com>
2017-11-14 12:26:30 -0500

    B3DivisorCount added comment and other print statement

--- src/unit2/assignment/B3DivisorCount.java
+++ src/unit2/assignment/B3DivisorCount.java
@@ -5,14 +5,15 @@ import codehs.*;
 public class B3DivisorCount extends ConsoleProgram {
 
     public void run() {
+        //Get userNumber from user
         int userNumber = readInt("Enter an integer you want the divisors for: ");
-        System.out.println("The divisors are:");
+        System.out.println("The divisors of " + userNumber + " are:");
 
+        //
         for(int counter = 1; counter <= userNumber; counter++){
             if(userNumber % counter == 0){
                 System.out.println(counter);
             }
         }
-
     }
 }
