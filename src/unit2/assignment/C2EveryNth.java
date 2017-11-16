623b31f2331429b6b8d806805dcea65c20a05c2d
xuj1210 <joshuax1210@gmail.com>
2017-11-16 12:50:55 -0500

    finished for loop to print the Nth letters of the string

--- src/unit2/assignment/C2EveryNth.java
+++ src/unit2/assignment/C2EveryNth.java
@@ -8,5 +8,10 @@ public class C2EveryNth extends ConsoleProgram {
         String userString = readLine("Enter a string: ");
         int everyNth = readInt("Enter an integer: ");
         int letterCount = 0;
+
+        for(int counter = 0; counter < userString.length(); counter += everyNth){
+            System.out.print(userString.substring(letterCount, letterCount + 1));
+            letterCount += everyNth;
+        }
     }
 }
