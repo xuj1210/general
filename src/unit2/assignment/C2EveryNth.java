73c378ebd6ff80e446af32ff824cda9fb628401f
xuj1210 <joshuax1210@gmail.com>
2017-11-16 12:47:45 -0500

    finished defining variables

--- src/unit2/assignment/C2EveryNth.java
+++ src/unit2/assignment/C2EveryNth.java
@@ -7,8 +7,6 @@ public class C2EveryNth extends ConsoleProgram {
     public void run() {
         String userString = readLine("Enter a string: ");
         int everyNth = readInt("Enter an integer: ");
-
-
-
+        int letterCount = 0;
     }
 }
