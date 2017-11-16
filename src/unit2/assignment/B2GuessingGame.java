18733d73342a5c9ab63e08b0ad66ed70e35a7b7d
xuj1210 <joshuax1210@gmail.com>
2017-11-16 12:57:39 -0500

    added comments for clarification

--- src/unit2/assignment/B2GuessingGame.java
+++ src/unit2/assignment/B2GuessingGame.java
@@ -1,8 +1,13 @@
 package unit2.assignment;
 import codehs.*;
 import java.util.Random;
-
+/*
+A program that secretly generates a number in between 1 and 100, and gives the user 5 attempts
+to guess what that number is.
+Author: Joshua Xu
+ */
 public class B2GuessingGame extends ConsoleProgram {
+
     public void run(){
         //New random
         Random newRandom = new Random();
@@ -12,7 +17,7 @@ public class B2GuessingGame extends ConsoleProgram {
         int userGuess = readInt("Guess a number: ");
         int counter = 1;
 
-        //While loop for user guesses
+        //While loop for user guesses and prints appropriate message
         while(counter < 6){
             if(counter == 5){
                 System.out.println("Nice Try");
