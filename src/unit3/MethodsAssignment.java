81b8c5276722057a9f7e133f56ff2882e12731bf
xuj1210 <joshuax1210@gmail.com>
2017-12-04 13:48:18 -0500

    evenlySpaced comments added

--- src/unit3/MethodsAssignment.java
+++ src/unit3/MethodsAssignment.java
@@ -13,13 +13,15 @@ public class MethodsAssignment {
         //Define isTrue
         boolean isTrue = false;
 
-        //
+        //Check if the numbers are evenly spaced and return true or false
         try{
             if(num2 - num1 == num3 - num2){
                 isTrue = true;
             }else{
                 isTrue = false;
             }
+
+        //Stop program if input provided is not an integer
         }catch(InputMismatchException i){
             System.out.println("Must input integers only. " + i.getMessage());
         }
