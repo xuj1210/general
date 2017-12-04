7a49ef217f525f72959133fa4aab49161f69339e
xuj1210 <joshuax1210@gmail.com>
2017-12-04 13:46:23 -0500

    test outputs and finished barebones of evenlySpaced

--- src/unit3/MethodsAssignment.java
+++ src/unit3/MethodsAssignment.java
@@ -4,12 +4,16 @@ import java.util.InputMismatchException;
 
 public class MethodsAssignment {
     public static void main(String[] args){
-        System.out.println("Test 1:");
-        evenlySpaced(1, 2, 3);
+        System.out.println("Test 1: Inputs: 1, 2, 3");
+        System.out.println("Expected result: True");
+        System.out.println("Actual result: " + evenlySpaced(1, 2, 3));
     }
 
     public static boolean evenlySpaced (int num1, int num2, int num3){
+        //Define isTrue
         boolean isTrue = false;
+
+        //
         try{
             if(num2 - num1 == num3 - num2){
                 isTrue = true;
@@ -17,7 +21,7 @@ public class MethodsAssignment {
                 isTrue = false;
             }
         }catch(InputMismatchException i){
-            System.out.println("Are the numbers evenly spaced: " + i.getMessage());
+            System.out.println("Must input integers only. " + i.getMessage());
         }
     return isTrue;
     }
