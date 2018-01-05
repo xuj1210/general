3352c7007ae2d2d21ade752407699257bc3a2684
Joshua Xu <joshuax1210@gmail.com>
2018-01-05 13:24:05 -0500

    fixed printing errors, added tests for shiftLeft3

--- src/unit4/ArrayAssignment.java
+++ src/unit4/ArrayAssignment.java
@@ -3,22 +3,38 @@ package unit4;
 public class ArrayAssignment {
     public static void main(String[] args) {
         //shiftLeft3()
-        System.out.println(shiftLeft3(1, 2, 3));
+        System.out.println("shiftLeft3() Tests:");
+        int[] shiftLeftTest1 = {1, 2, 3};
+        shiftLeft3(shiftLeftTest1);
 
+        int[] shiftLeftTest2 = {10, 10, 10};
+        shiftLeft3(shiftLeftTest2);
+
+        int[] shiftLeftTest3 = {9, 7, 8};
+        shiftLeft3(shiftLeftTest3);
     }
 
-    public static int[] shiftLeft3(int num1, int num2, int num3) {
-        int[] numberArray = {num1, num2, num3};
-
-        int firstNum = num1;
-        int secondNum = num2;
-        int thirdNum = num3;
+    public static int[] shiftLeft3(int[] numberArray) {
+        int firstNum = numberArray[0];
+        int secondNum = numberArray[1];
+        int thirdNum = numberArray[2];
 
         numberArray[0] = secondNum;
         numberArray[1] = thirdNum;
         numberArray[2] = firstNum;
-        System.out.println(numberArray);
-        return numberArray;
 
+        for (int i = 0; i < numberArray.length; i++){
+            if (i == numberArray.length - 1){
+                System.out.println(numberArray[i]);
+                break;
+            }
+            System.out.print(numberArray[i] + ", ");
+        }
+
+        return numberArray;
     }
 }
+
+    public static int[] after4(int[] numberArray){
+
+    }
