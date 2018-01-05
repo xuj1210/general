1542134798f5416977d05d76e148807cc95ed220
Joshua Xu <joshuax1210@gmail.com>
2018-01-05 15:03:39 -0500

    finished after4

--- src/unit4/ArrayAssignment.java
+++ src/unit4/ArrayAssignment.java
@@ -12,6 +12,18 @@ public class ArrayAssignment {
 
         int[] shiftLeftTest3 = {9, 7, 8};
         shiftLeft3(shiftLeftTest3);
+
+        System.out.println("");
+
+        System.out.println("after4() Tests:");
+        int[] after4Test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        after4(after4Test1);
+
+        int[] after4Test2 = {4, 3, 2, 1, 10, 10, 20, 30, 70};
+        after4(after4Test2);
+
+        int[] after4Test3 = {10, 9, 8, 7, 6, 5, 4, 3};
+        after4(after4Test3);
     }
 
     public static int[] shiftLeft3(int[] numberArray) {
@@ -33,8 +45,22 @@ public class ArrayAssignment {
 
         return numberArray;
     }
+    public static void after4(int[] numberArray){
+        int last4Index = 0;
+        for(int index = numberArray.length - 1; index >= 0; index--){
+            if(numberArray[index] == 4){
+                last4Index = index;
+                break;
+            }
+        }
+        for (int i = last4Index + 1; i <= numberArray.length - 1; i++){
+            if (i == numberArray.length - 1){
+                System.out.println(numberArray[i]);
+                break;
+            }
+            System.out.print(numberArray[i] + ", ");
+        }
+    }
+
 }
 
-    public static int[] after4(int[] numberArray){
-
-    }
