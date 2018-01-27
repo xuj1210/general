f8dd2857cb1ad5deb82d4d04e90176b44cc39e44
Joshua Xu <joshuax1210@gmail.com>
2018-01-26 23:59:57 -0500

    finished everything, added key and trap room

--- src/grade11CPT.java
+++ src/grade11CPT.java
@@ -13,19 +13,20 @@ public class grade11CPT {
     static boolean done = false;
     static int passwordAttempts = 0;
     static boolean passwordCorrect;
+    static boolean hasKey;
 
     public static void main(String[] args){
-        roomNames = new String[]{"", "Entrance", "", "", "", "Vine Room", "Sun Lit Hallway", "Hidden Room", "Dead End", "Plant Room", "Cracked Room", "Bone Room", "", "Throne Room", "Statue Hallway", "Small Room", "", "Tree Hallway", "Ritual Room", "Chest Room", "", "Plain Room", "", ""};
+        roomNames = new String[]{"", "Entrance", "", "", "", "Vine Room", "Sun Lit Hallway", "Hidden Room", "Dead End", "Plant Room", "Cracked Room", "Bone Room", "", "Trap Room", "Statue Hallway", "Small Room", "", "Tree Hallway", "Ritual Room", "Chest Room", "", "Plain Room", "", ""};
         roomDescriptions = new String[22];
-        roomDescriptions[1] = "You are in a long lost jungle temple looking for the rumored chest of the ancient king. In order to plunder the goods, you must solve\nthe password that unlocks the door to the room with the chest. Clues can be found within the various rooms. Good luck! Start by going North. (n): ";
+        roomDescriptions[1] = "You are in a long lost jungle temple looking for the rumored chest of the ancient king. In order to plunder the goods, you must find a key located in one of the rooms\nwithin the temple. As well you must solve the password that unlocks the door to the room with the chest. Clues can be found within the various rooms. Good luck! Start by going North. (n): ";
         roomDescriptions[5] = "You are in a room full of vines, with hallways leading North and East.\nWhich way do you go? (n / e): ";
         roomDescriptions[6] = "This is a sun lit hallway, with passages leading North, East, and West.\nWhich way do you go? (n / e / w): ";
         roomDescriptions[7] = "You find a note on the ground. It says, “The secret word is an adjective.” There are no new passages.\nYou must go back west. (w): ";
         roomDescriptions[8] = "This room is a dead end. However, there is a note on the ground. It reads, “The last letter of the password is ‘n’.\nYou may only go back to the last room you were in. (e): ";
         roomDescriptions[9] = "You are in a large room with vegetation sprouting from the ground. There are doors leading East, South and West.\nWhich way do you go? (e / s / w): ";
         roomDescriptions[10] = "You are in a room with large cracks running along the walls and ceiling, with sunlight seeping through the cracks.\nThere are doors leading North, East, South, and West.\nWhich way do you go? (n / e / s / w): ";
-        roomDescriptions[11] = "There are skeletons scattered everywhere around the ground. “The password is one of the components of a unique arc within\nthe sky that appears after rain.” There are doors to the North and West.\nWhich way do you go? (n / w): ";
-        roomDescriptions[13] = "There is a throne in a corner of the room. There are areas to the North and East.\nWhich way do you go? (n / e): ";
+        roomDescriptions[11] = "There are skeletons scattered everywhere around the ground. You also find the key and pick it up. There are doors to the North and West.\nWhich way do you go? (n / w): ";
+        roomDescriptions[13] = "Congratulations! You correctly answered the password! There are doors leading to the north and east. Which way do you want to go? (n / e): ";
         roomDescriptions[14] = "You are in a hallway with statues everywhere. There are exits to the North, East, South, and West.\nWhich way do you go? (n / e / s / w): ";
         roomDescriptions[15] = "You are in a room much smaller than normal. There are passages leading to the South and West.\nWhich way do you go? (s / w): ";
         roomDescriptions[17] = "You are in a hallway with a large tree in the center. Inscribed on the tree there is a message which reads, “The password can be used to represent life.” There are areas to the North, East, and South.\nWhich way do you go? (n / e / s): ";
@@ -51,14 +52,11 @@ public class grade11CPT {
 
         currentRoom = 1;
 
-                while (!done && passwordAttempts < 3) {
+                while (!done && passwordAttempts < 3 ) {
                     roomDescription(currentRoom);
                     done = endCheck(currentRoom);
                     userSelectedRoomStr = userInput();
                     System.out.println("");
-                    if (passwordAttempts == 1) {
-                        System.out.println("Careful! You only have one more attempt for the password if you get this one wrong!");
-                    }
 
                     if (userSelectedRoomStr.equalsIgnoreCase("n")) {
                         nextRoom = roomDirections[currentRoom][0];
@@ -76,10 +74,13 @@ public class grade11CPT {
                         } else if (nextRoom == 19) {
                             passwordChecker();
                             System.out.println("");
-                            if (passwordCorrect) {
+                            if (passwordCorrect && hasKey) {
                                 currentRoom = nextRoom;
                                 roomDescription(currentRoom);
                                 break;
+                            } else if (passwordCorrect && !hasKey){
+                                System.out.println("You opened the door but you do not have the key for the chest! Keep looking around the temple to find the key.");
+                                System.out.println("");
                             } else if (passwordAttempts < 2) {
                                 currentRoom = 18;
                                 System.out.println("You got the password wrong. " + (2 - passwordAttempts) + " attempt(s) left. Look around the temple for more clues.");
@@ -89,6 +90,9 @@ public class grade11CPT {
                                 System.out.println("You got the password incorrect too many times and the temple began to crumble, forcing you to evacuate.\nBetter luck next time!");
                                 done = true;
                             }
+                        } else if (nextRoom == 11) {
+                            currentRoom = nextRoom;
+                            hasKey = true;
                         } else {
                             currentRoom = nextRoom;
                         }
@@ -97,6 +101,17 @@ public class grade11CPT {
                         if (nextRoom == -1) {
                             System.out.println("Invalid input. You cannot go there.");
                             System.out.println("");
+                        } else if (nextRoom == 11) {
+                            currentRoom = nextRoom;
+                            hasKey = true;
+                        } else if (nextRoom == 13){
+                            boolean riddleAnswered = trapRoom();
+
+                            if (riddleAnswered) {
+                                currentRoom = nextRoom;
+                            } else {
+                                break;
+                            }
                         } else {
                             currentRoom = nextRoom;
                         }
@@ -105,6 +120,14 @@ public class grade11CPT {
                         if (nextRoom == -1) {
                             System.out.println("Invalid input. You cannot go there.");
                             System.out.println("");
+                        } else if (nextRoom == 13) {
+                            boolean riddleAnswered = trapRoom();
+
+                            if(riddleAnswered) {
+                                currentRoom = nextRoom;
+                            } else {
+                                break;
+                            }
                         } else {
                             currentRoom = nextRoom;
                         }
@@ -147,6 +170,29 @@ public class grade11CPT {
         }
 
         return passwordCorrect;
+    }
+
+    public static boolean trapRoom(){
+        boolean escaped = false;
+        String userAnswer;
+        int riddleAttempts = 0;
+        System.out.println("Uh oh! You are in the trap room! On the walls, there are writings which say, 'In order to escape, you must solve the following riddle:\nMy rings are not worth much, but they do tell my age. What am I?: ' ");
+        while(riddleAttempts < 3) {
+            userAnswer = userInput();
+
+            if (userAnswer.equalsIgnoreCase("tree")) {
+                escaped = true;
+                currentRoom = nextRoom;
+                break;
+            } else if (riddleAttempts <= 1) {
+                System.out.println("Nothing happened. Looks like you got the password wrong. You have " + (2 - riddleAttempts) + " attempt(s) left.");
+                riddleAttempts++;
+            } else {
+                System.out.println("You failed to solve the riddle correctly in the allotted attempts, and the temple crashed around you. Better luck next time!");
+                break;
+            }
+        }
+        return escaped;
     }
 
 }
